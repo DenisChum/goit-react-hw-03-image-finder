18ff87dfbdb7122e9e0f1c2b579fb34b46246c54
DenisChum <105285389+DenisChum@users.noreply.github.com>
2022-10-10 13:37:39 +0200

    add

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import Loader from 'react-loader-spinner';
+import css from './Spinner.module.css';
+
+export default function Spinner () {
+  return (
+    <div className={css.container}>
+      <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+    </div>
+  );
+}
+
+
